f06d50173f77c0c6e1e9c5be9fa6b597d887ed00
(Estudiante) Michelle Stephania Altamirano Armijos <michelle.altamirano@udla.edu.ec>
2026-04-28 16:45:32 -0500

    primer commit: agregar main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const * argv[]) {
+    int a, b;
+    printf("Ingrese 1er numero: ");
+    scanf("%d", &a);
+    printf("Ingrese 2do numero: ");
+    scanf("%d", &b);
+    return 0;
+}
